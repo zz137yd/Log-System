bcf777009849cabc55066f09e4cde1136642eb91
zz137yd <astronomy5763294@sina.com>
2024-03-10 16:39:02 +0800

    Utility tools class

--- util.hpp
+++ util.hpp
@@ -1,11 +1,11 @@
 #ifndef __M_UTIL_H__
 #define __M_UTIL_H__
 
-/* 实用工具类的实现:
-    1、获取系统时间
-    2、判断文件是否存在
-    3、获取文件所在路径
-    4、创建目录
+/*  Utility tools:
+    1、Get system time
+    2、Determine whether the file exists
+    3、Get file path
+    4、Create directory
 */
 
 #include <iostream>
@@ -21,65 +21,69 @@ namespace Logs
         class Date
         {
         public:
-            static time_t now() {return time(nullptr); }//获取系统时间
+            static time_t now() {return time(nullptr); }//Get system time
         };
 
         class File
         {
         public:
-            //判断文件是否存在
+            //Determine whether the file exists
             static bool exists(const std::string& name)
             {
-                //return (access(pathname.c_str(), F_OK) == 0);//Linux下的接口, 后面填F_OK可以用来检测文件有没有, 有就返回0, 没有返回-1; F_OK表示检查文件是否存在
+                //Linux interface, filling in F_OK at the end can be used to check whether the file exists. If it does, return 0, if not, return -1
+                //return (access(pathname.c_str(), F_OK) == 0);
 
-                //通用接口(stat遵循POSIX标准) 
+                //Common interface (stat follows POSIX standard) 
                 struct stat st;
-                //头文件sys/stat.h，stat用来获取文件属性, 获取得到就说明存在
+                //Header: sys/stat.h, stat is used to obtain file attributes. If obtained, it means that file exists.
                 return stat(name.c_str(), &st) == 0;
             }
 
-            //获取文件所在路径
+            //Get file path
             static std::string path(const std::string& pathname)
             {
                 if(pathname.empty()) return ".";
-                //找文件名之前的最后一个文件分隔符/, /\\是Windows, Linux通用写法, 查找斜杠和反斜杠中任意一个, 所以通用
+                //Find the last "/" before the file name /, /\\ is common in Windows and Linux
                 size_t pos = pathname.find_last_of("/\\");
-                if(pos == std::string::npos) return ".";//说明在当前目录
-                return pathname.substr(0, pos + 1);//pos + 1包含斜杠位置
+                if(pos == std::string::npos) return ".";//Can be seen in current directory
+                return pathname.substr(0, pos + 1);//pos + 1  Includes "/"
             }
             
-            //创建目录
+            //Create directory
             static void createDirectory(const std::string& path)
             {
-                if(path.empty() || exists(path)) return ;//参数为空或者已经存在了就退出
-                size_t pos = 0, idx = 0;//idx表示查找开始的位置
+                if(path.empty() || exists(path)) return ;//Exit if the parameter is empty or already exists
+                size_t pos = 0, idx = 0;//idx: the location where the search starts
                 while (idx < path.size())
                 {
                     pos = path.find_first_of("/\\", idx);
-                    if (pos == std::string::npos)//头文件sys/stat.h, sys/types.h
+                    if (pos == std::string::npos)//Header: sys/stat.h, sys/types.h
                     {
                         mkdir(path.c_str(), 0777);
                         return ;
                     }
-                    if(pos == idx)//这里表明已经结束了
+                    if(pos == idx)
                     {
                         idx = pos + 1;
                         continue;
                     }
 
-                    //下句要拿到完整的路径, 所以要从0开始, 后面的pos表示就是当前检查到的斜杠位置, +1包含斜杠
-                    //这里不+1, 也就是说, 比如/a/b, 这里就只取/a, 先创建a目录, 再次循环后会得到/b
+                    //The next sentence needs to get the complete path, so it starts from 0
+                    //The following pos indicates the currently checked "/" position, and +1 includes "/"
+                    //No +1 here, that is to say
+                    //for example, /a/b, here we only take /a, create the a directory first, and after looping again, we will get /b
                     std::string subdir = path.substr(0, pos);
                     //std::cout << subdir << std::endl;
 
-                    //先判断当前有没有subdir这个目录
-                    //if (subdir == "." || subdir == "..") {idx = pos + 1; continue;}//去掉了, 因为下句exists也会判断这个
-                    if(exists(subdir) == true)//.和..都会返回true 
+                    //Determine whether there is currently a subdir directory
+                    //if (subdir == "." || subdir == "..") {idx = pos + 1; continue;}//Removed, because "exists" will also judge this
+
+                    if(exists(subdir) == true)//. and .. both return true
                     {
                         idx = pos + 1;
                         continue;
                     }
-                    //不存在就创建
+                    //Create if it doesn't exist
                     mkdir(subdir.c_str(), 0777);
                     idx = pos + 1;
                 }
