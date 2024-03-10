1d1c48930e7357f42d1e2fff3a33cb03ff99ba9d
zz137yd <astronomy5763294@sina.com>
2024-03-10 20:28:06 +0800

    Double buffer class called in asynchronous logger

--- buffer.hpp
+++ buffer.hpp
@@ -1,4 +1,4 @@
-/*实现异步日志缓冲区*/
+/*Implement asynchronous log buffer*/
 
 #ifndef __M_BUF_H__
 #define __M_BUF_H__
@@ -10,35 +10,36 @@
 namespace Logs
 {
     #define DEFAULT_BUFFER_SIZE (4 * 1024 * 1024)//4MB
-    #define THRESHOLD_BUFFER_SIZE (10 * 1024 * 1024)//扩容用的阈值, 不超过就翻倍增长, 超过就线性增长
-    #define INCREMENT_BUFFER_SIZE (1 * 1024 * 1024)//线性增长
+    //The threshold used for expansion. If it doesn't exceed, it will double. If it exceeds, it will grow linearly
+    #define THRESHOLD_BUFFER_SIZE (10 * 1024 * 1024)
+    #define INCREMENT_BUFFER_SIZE (1 * 1024 * 1024)//linear growth
 
     class Buffer
     {
     public:
         Buffer() :_buffer(DEFAULT_BUFFER_SIZE), _reader_idx(0), _writer_idx(0) {}
 
-        //判断缓冲区是否为空
+        //Determine whether the buffer is empty
         bool empty() {return _reader_idx == _writer_idx; }
 
-        //返回可读数据的起始地址
+        //Returns the starting address of readable data
         const char* begin() {return &_buffer[_reader_idx]; }
 
-        //返回可读数据的长度
+        //Returns the length of readable data
         size_t readAbleSize()
         {
-            //当前缓冲区是双缓冲区, 处理完就交换, 不做循环使用
+            //The current buffer is a double buffer, which will be exchanged after processing and will not be used in loop
             return _writer_idx - _reader_idx;
         }
 
-        //返回可写数据的长度
+        //Returns the length of writable data
         size_t writeAbleSize()
         {
-            //这个接口仅为固定大小缓冲区提供
+            //This interface is only provided for fixed size buffer
             return _buffer.size() - _writer_idx;
         }
 
-        //对两个缓冲区进行数据交换
+        //Exchange data between two buffers
         void swap(Buffer& buf)
         {
             _buffer.swap(buf._buffer);
@@ -46,36 +47,36 @@ namespace Logs
             std::swap(_writer_idx, buf._writer_idx);
         }
 
-        //向缓冲区写入数据
+        //Write data to buffer
         void push(const char* data, size_t len)
         {
-            //assert(len <= writeAbleSize());//阻塞
+            //assert(len <= writeAbleSize());//Block
 
-            //1、空间不够就扩容
-            ensureEnoughSize(len);//动态扩容
+            //1、Not enough space, then expand
+            ensureEnoughSize(len);//Dynamic expansion
             
-            //2、将数据拷贝进缓冲区
+            //2、Copy data into buffer
             std::copy(data, data + len, &_buffer[_writer_idx]);
-            //3、当前写入位置向后偏移
-            //第一步就保证了+len不会超过缓冲区总大小
+            //3、Offset backward from current writing position
+            //The first step ensures that +len will not exceed the total buffer size
             _writer_idx += len;
         } 
 
-        //对读写指针进行向后偏移操作
-        void pop(size_t len)//读取数据后向后偏移moveReader
+        //Offset the read and write pointers backward
+        void pop(size_t len)//Offset moveReader backward after reading data
         {
             assert(len <= readAbleSize());
             _reader_idx += len;
         }
 
-        //没有数据可读时重置读写位置, 初始化缓冲区
+        //When there is no data to read, reset the read and write position and initialize the buffer
         void reset()
         {
-            _writer_idx = 0;//缓冲区所有空间都是空闲的
-            _reader_idx = 0;//与_writer_idx相等说明没有数据可读
+            _writer_idx = 0;//All space in the buffer is free
+            _reader_idx = 0;//Equality with _writer_idx means no data is readable
         }
     private:
-        //扩容缓冲区
+        //Expansd buffer
         void ensureEnoughSize(size_t len)
         {
             if(len <= writeAbleSize()) return ;
