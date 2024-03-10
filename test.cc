ec04224808e275906cf68c0d976df27566487470
zz137yd <astronomy5763294@sina.com>
2024-03-10 21:06:25 +0800

    Functions test file

--- test.cc
+++ test.cc
@@ -4,34 +4,34 @@ void test_log()
 {
     //Time
     /*Logs::Logger::ptr logger = Logs::LoggerManager::getInstance().getLogger("async_logger");
-    logger->debug(__FILE__, __LINE__, "%s", "测试日志d");
-    logger->info(__FILE__, __LINE__, "%s", "测试日志i");
-    logger->warn(__FILE__, __LINE__, "%s", "测试日志w");
-    logger->error(__FILE__, __LINE__, "%s", "测试日志e");
-    logger->fatal(__FILE__, __LINE__, "%s", "测试日志f");
+    logger->debug(__FILE__, __LINE__, "%s", "test logd");
+    logger->info(__FILE__, __LINE__, "%s", "test logi");
+    logger->warn(__FILE__, __LINE__, "%s", "test logw");
+    logger->error(__FILE__, __LINE__, "%s", "test loge");
+    logger->fatal(__FILE__, __LINE__, "%s", "test logf");
     size_t count = 0;
-    std::string str = "测试日志-";
+    std::string str = "test log-";
     while(count < 470000)
     {
         std::string tmp = str + std::to_string(count++);
         logger->fatal(__FILE__, __LINE__, tmp.c_str());
     }*/
-    /*DEBUG("%s", "测试日志");
-    INFO("%s", "测试日志");
-    WARN("%s", "测试日志");
-    ERROR("%s", "测试日志");
-    FATAL("%s", "测试日志");
+    /*DEBUG("%s", "test log");
+    INFO("%s", "test log");
+    WARN("%s", "test log");
+    ERROR("%s", "test log");
+    FATAL("%s", "test log");
     size_t count = 0;
     while(count < 470000)
     {
-        FATAL("测试日志-%d", count++);
+        FATAL("test log-%d", count++);
     }*/
 
 
     //Size
     /*size_t cursize = 0, count = 0;
-    std::string str = "测试日志-";
-    while(cursize < 1024 * 1024 * 10)//小于10MB就持续写入
+    std::string str = "test log-";
+    while(cursize < 1024 * 1024 * 10)//Keep writing if it's less than 10MB
     {
         std::string tmp = str + std::to_string(count++);
         logger->fatal(__FILE__, __LINE__, tmp.c_str());
@@ -41,11 +41,13 @@ void test_log()
 
 int main()
 {
-    //传给path函数的string，./后面跟上路径就是在当前目录创建，直接写/路径，就是把路径创建和root同目录下
+    //In the string passed to the path function, ./ followed by the path means it will be created in the current directory
+    //Writing /path directly will create the path in the same directory as root
+
     //std::string pn = "./lf/a/b/c.cc";
     //Logs::LogUtil::File::createDirectory(Logs::LogUtil::File::path(pn));
 
-    //Logs::LogMsg msg(Logs::LogLevel::value::Info, 47, "main.c", "root", "格式化功能测试");
+    //Logs::LogMsg msg(Logs::LogLevel::value::Info, 47, "main.c", "root", "format function test");
     //Logs::Formatter fmt;
     //Logs::Formatter fmt("abc%%abc%g%g%g[%d{%H:%M:%S}] %m%n%g");
     //std::string str = fmt.format(msg);
@@ -58,7 +60,7 @@ int main()
     /*Logs::LogSink::ptr roll_lsp = Logs::SinkFactory::create<Logs::RollBySizeSink>("./logfile/roll-", 1024 * 1024);
     size_t cursize = 0;
     size_t count = 0;
-    while(cursize < 1024 * 1024 * 10)//小于10MB就持续写入
+    while(cursize < 1024 * 1024 * 10)//Keep writing if it's less than 10MB
     {
         std::string tmp = std::to_string(count++) + " " + str;
         roll_lsp->log(tmp.c_str(), tmp.size());
@@ -81,15 +83,15 @@ int main()
     builder->buildSink<Logs::FileSink>("./logfile/test.log");
     builder->buildSink<Logs::StdoutSink>();
     Logs::Logger::ptr logger = builder->build();
-    logger->debug(__FILE__, __LINE__, "%s", "测试日志d");
-    logger->info(__FILE__, __LINE__, "%s", "测试日志i");
-    logger->warn(__FILE__, __LINE__, "%s", "测试日志w");
-    logger->error(__FILE__, __LINE__, "%s", "测试日志e");
-    logger->fatal(__FILE__, __LINE__, "%s", "测试日志f");
+    logger->debug(__FILE__, __LINE__, "%s", "test logd");
+    logger->info(__FILE__, __LINE__, "%s", "test logi");
+    logger->warn(__FILE__, __LINE__, "%s", "test logw");
+    logger->error(__FILE__, __LINE__, "%s", "test loge");
+    logger->fatal(__FILE__, __LINE__, "%s", "test logf");
 
     size_t cursize = 0, count = 0;
-    std::string str = "测试日志-";
-    while(cursize < 1024 * 1024 * 10)//小于10MB就持续写入
+    std::string str = "test log-";
+    while(cursize < 1024 * 1024 * 10)//Keep writing if it's less than 10MB
     {
         std::string tmp = std::to_string(count++) + str;
         logger->fatal(__FILE__, __LINE__, tmp.c_str());
@@ -107,14 +109,14 @@ int main()
     builder->buildSink<Logs::StdoutSink>();
     Logs::Logger::ptr logger = builder->build();
 
-    logger->debug(__FILE__, __LINE__, "%s", "测试日志d");
-    logger->info(__FILE__, __LINE__, "%s", "测试日志i");
-    logger->warn(__FILE__, __LINE__, "%s", "测试日志w");
-    logger->error(__FILE__, __LINE__, "%s", "测试日志e");
-    logger->fatal(__FILE__, __LINE__, "%s", "测试日志f");
+    logger->debug(__FILE__, __LINE__, "%s", "test logd");
+    logger->info(__FILE__, __LINE__, "%s", "test logi");
+    logger->warn(__FILE__, __LINE__, "%s", "test logw");
+    logger->error(__FILE__, __LINE__, "%s", "test loge");
+    logger->fatal(__FILE__, __LINE__, "%s", "test logf");
 
     size_t count = 0;
-    std::string str = "测试日志-";
+    std::string str = "test log-";
     while(count < 500000)
     {
         std::string tmp = std::to_string(count++) + str;
